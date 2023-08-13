0993853294bc1a7b568a2c111812ce996c76d347
TimbleTam <thehyperdragon@gmail.com>
2023-08-13 19:59:01 +0100

    sync

--- main.js
+++ main.js
@@ -1,58 +1,75 @@
-
 const software_icon_home = document.querySelector("#icons-home");
 
-window.onload = function() {
+window.onload = function () {
     transition_icons(0);
-}
+};
 
-function transition_icons(ind)
-{
+function transition_icons(ind) {
     software_icon_home.removeChild(software_icon_home.firstChild);
-    
-    const new_pic = document.createElement('img');
+
+    const new_pic = document.createElement("img");
     new_pic.src = ind + ".png";
     new_pic.classList.add("icon-pic-new");
     new_pic.type = "icon-pic-new";
-    if(ind >= 7)
-    {
+    if (ind >= 7) {
         ind = 0;
-    }
-    else
-    {
+    } else {
         ind += 1;
     }
 
-    new_pic.addEventListener('animationend', () => {
+    new_pic.addEventListener("animationend", () => {
         transition_icons(ind);
     });
 
     software_icon_home.appendChild(new_pic);
 }
 
-
 const elts = {
     text1: document.getElementById("text1"),
-    text2: document.getElementById("text2")
+    text2: document.getElementById("text2"),
 };
 
 const texts = [
-    
     "Games",
-    
+
     "Stories",
     "Models",
 
-    
     "Scripts",
     "Code",
     "UX",
     "Data Structures",
     "Algo",
-    
-    
-    "People Happy",
 
+    "People Happy",
 ];
+
+const mobile_animated = document.getElementById("morph-text-mobile");
+const back_animated = document.getElementById("introduction");
+
+function togglenav() {
+    document.getElementsByClassName("burger-toggle")[0].classList.toggle("active");
+    document.getElementsByClassName("toggle-nav")[0].classList.toggle("active");
+}
+
+
+mobile_animated.addEventListener("animationend", () => {
+    mobile_animated.style.animationName = "";
+    mobile_animated.style.animationDelay = "0s";
+    mobile_animated.innerHTML = texts[(Math.random() * (texts.length - 1)).toFixed()];
+    setTimeout(() => {mobile_animated.style.animationName = "MobileMorphAnim"; });
+});
+
+back_animated.addEventListener("animationend", () => {
+
+    back_animated.style.animationName = "";
+    const ind = ((Math.random() * 11) + 1).toFixed();
+    back_animated.style.backgroundImage = "linear-gradient(to bottom, rgba(109, 109, 109, 0.5), rgba(23, 23, 23, 1)), url('me pic " + ind.toString() + ".jpg')";
+
+    setTimeout(() => {back_animated.style.animationName = "BackAnim"; }, 1);
+});
+
+
 
 const morphTime = 0.75;
 const cooldownTime = 0.17;
@@ -123,15 +140,5 @@ function animate() {
 }
 
 animate();
-
-
-
-const map = document.querySelector('.map');
-const observer = new IntersectionObserver(entries => {
-  map.classList.toggle( 'map-animation', entries[0].isIntersecting );
-});
-
-observer.observe( map );
-
 
 
